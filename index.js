3fa189f2e187e6479fabf44cb49b134f4304874b
Samueelx <gsammygitau@gmail.com>
2022-12-12 07:36:36 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const submitData = (name, email) => {
+    const dataObject = {
+        name: name,
+        email: email       
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(dataObject)
+    }).then(res => res.json()).then( (data) => {
+        const p = document.createElement('p');
+        p.textContent = data.id;
+        document.body.appendChild(p);
+    }).catch((error) => {
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        document.body.appendChild(p);
+    });
+}
